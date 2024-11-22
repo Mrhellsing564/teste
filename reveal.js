3441c948982b6e43eee2249f516b7f272ae0ce50
HIAGO EMANUEL ANGELO RIBEIRO MARQUES <58947830+Mrhellsing564@users.noreply.github.com>
2024-11-22 02:38:16 -0300

    Add files via upload

---
+++ reveal.js
@@ -0,0 +1,163 @@
+
+
+window.revelar = ScrollReveal({reset:true})
+
+
+revelar.reveal('.efeito-paragrafo1',
+    {
+        duration:2000,
+        rotate:{x:100, y:100, z:0},
+        delay:300
+    
+    })
+
+
+//texto do site//
+revelar.reveal('.efeito-txt',
+{
+    duration:2000,
+    distance:'90px'
+
+})
+
+
+revelar.reveal('.efeito-img',
+    {
+        duration:2000,
+        distance:'90px',
+        delay:500,
+    
+    })
+
+    revelar.reveal('.efeito-img1',
+        {
+            duration:2000,
+            distance:'90px',
+            delay:500,
+        
+        })
+
+//fim dos teste agora vou falar do triassico
+
+revelar.reveal('.efeito-tri',
+    {
+        duration:2000,
+        distance:'90px'
+    
+    })
+
+//clima tri
+
+revelar.reveal('.efeito-clima-tri',
+    {
+        duration:2000,
+        distance:'90px',
+        delay:400,
+        origin:'left'
+    })
+
+//datacao2
+revelar.reveal('.efeito-datacao2',
+    {
+        duration:2000,
+        distance:'90px',
+        origin:'bottom'
+    })
+
+//especie titulo
+
+//fim-tri
+revelar.reveal('.efeito-fim-tri',
+    {
+        duration:2000,
+        distance:'90px',
+        origin:'left'
+    })
+
+//inicio do jura
+
+revelar.reveal('.efeito-jura',
+    {
+        duration:2000,
+        distance:'90px',
+    })
+
+//clima-jura
+    revelar.reveal('.efeito-clima-jura',
+        {
+            duration:4000,
+            distance:'90px',
+            origin:'left'
+        })
+//dataçao-jura
+
+
+revelar.reveal('.efeito-datacao-jura',
+    {
+        duration:4000,
+        distance:'90px',
+        origin:'left'
+    })
+
+//fim-tri
+
+revelar.reveal('.efeito-fim-jura',
+    {
+        duration:2000,
+        distance:'90px',
+        origin:'rigth'
+    })
+
+//inicio do cretaciooooo
+
+revelar.reveal('.efeito-creta',
+    {
+        duration:2000,
+        distance:'90px',
+        origin:'bottom'
+    })
+
+//clima-creta
+
+revelar.reveal('.efeito-clima-creta',
+    {
+        duration:4000,
+        distance:'90px',
+        origin:'left',
+        delay:230
+    })
+
+//data-creta
+
+revelar.reveal('.efeito-data-creta',
+    {
+        duration:2000,
+        distance:'90px',
+    })
+
+//extinçao
+revelar.reveal('.efeito-extino1',
+    {
+        duration:2000,
+        distance:'90px',
+        origin:'left'
+    })
+
+revelar.reveal('.efeito-extino2',
+    {
+        duration:2000,
+        distance:'90px',
+        origin:'left'
+    })
+
+revelar.reveal('.efeito-extin-img',
+    {
+        duration:2000,
+        distance:'90px',
+    })
+
+revelar.reveal('.efeito-extin-img2',
+    {
+        duration:2000,
+        distance:'90px',
+    })
